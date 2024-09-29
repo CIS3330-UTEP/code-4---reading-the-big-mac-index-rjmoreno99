b009eca6c0e807d8d1c66623a45aa1095d2b6361
rjmoreno99 <rjmoreno@miners.utep.edu>
2024-09-29 07:32:55 -0600

    Update code_4.py
    get_big_mac_price_by_year done

--- code_4.py
+++ code_4.py
@@ -1,18 +1,25 @@
 import csv
 import pandas as pd
 big_mac_file = './big-mac-full-index.csv'
+df = pd.read_csv(big_mac_file)
 
 def get_big_mac_price_by_year(year,country_code):
-    pass # Remove this line and code your function
+    df = pd.read_csv(big_mac_file)
+    d = df[(df['iso_a3'] == country_code.upper()) & (df['date'].str[:4] == str(year))]
+    mean_by_year = round(float(d['dollar_price'].mean()), 2)
+
+    return mean_by_year
+
 
 def get_big_mac_price_by_country(country_code):
-    pass # Remove this line and code your function
+    pass # Mean
 
 def get_the_cheapest_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    pass # Min
 
 def get_the_most_expensive_big_mac_price_by_year(year):
-    pass # Remove this line and code your function
+    pass # Max
 
 if __name__ == "__main__":
-    pass # Remove this line and code your user interface
+    mean_by_year = get_big_mac_price_by_year(2010,'arg')
+    print(mean_by_year)
